9ffd93e762a756bc5db01231b10279d70bd82ac0
ATG <jpaps2124@gmail.com>
2020-05-27 02:54:17 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,90 +1,145 @@
+-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+--		   	Created By: qalle-fivem AKA qalle		      --
+--			 Protected By: ATG-Github AKA ATG			  --
+-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+
 ESX                = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterCommand("jail", function(src, args, raw)
+function checkIfLegit(source, target)
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	--				Let's grab our data...					  --
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	local src, tgt = source, target;
+	local xSrc, xTgt = ESX.GetPlayerFromId(src), ESX.GetPlayerFromId(tgt);
+	local srcIdent, tgtIdent = xSrc.identifier, xTgt.identifier;
+	local srcJob = xSrc.job.name;
+	local tgtJob = xTgt.job.name;
+	local srcGroup = xSrc.getGroup();
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	--				Let's define legitimacy...			      --
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	local legit = {
+		["legit"] = true,
+		["reason"] = "No flags found."
+	};
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	--				Let's test for legitimacy!			      --
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	if srcJob ~= "police" then
+		if srcGroup ~= "admin" and srcGroup ~= "superadmin" then
+			legit = {
+				["legit"] = false,
+				["reason"] = "Source does not have the police job, and is not staff."
+			}
+			return legit
+		end
+	end
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	--		     If we've made it here, it's legit!           --
+	-- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~= --
+	return legit
+end
 
-	local xPlayer = ESX.GetPlayerFromId(src)
+RegisterCommand("jail", function(source, args, raw)
+	local src, tgt = source, tonumber(args[1]);
+	local jailTime, jailReason = tonumber(args[2]), args[3];
 
-	if xPlayer["job"]["name"] == "police" then
-
-		local jailPlayer = args[1]
-		local jailTime = tonumber(args[2])
-		local jailReason = args[3]
-
-		if GetPlayerName(jailPlayer) ~= nil then
-
+	if GetPlayerName(tgt) ~= nil then
+		local legit = checkIfLegit(src, tgt);
+		if legit["legit"] == true then
 			if jailTime ~= nil then
-				JailPlayer(jailPlayer, jailTime)
-
-				TriggerClientEvent("esx:showNotification", src, GetPlayerName(jailPlayer) .. " Jailed for " .. jailTime .. " minutes!")
-				
+				JailPlayer(tgt, jailTime)
 				if args[3] ~= nil then
-					GetRPName(jailPlayer, function(Firstname, Lastname)
-						TriggerClientEvent('chat:addMessage', -1, { args = { "JUDGE",  Firstname .. " " .. Lastname .. " Is now in jail for the reason: " .. args[3] }, color = { 249, 166, 0 } })
+					GetRPName(tgt, function(Firstname, Lastname)
+						TriggerClientEvent("esx:showNotification", src, Firstname .. " " .. Lastname .. " has been sentenced to " .. jailTime .. " minutes in jail.")
+						TriggerClientEvent('chat:addMessage', -1, { args = { "JUDGE",  Firstname .. " " .. Lastname .. " has been sentenced to " .. jailTime .. " minutes in jail for the charges of " .. jailReason .. "." }, color = { 249, 166, 0 } })
 					end)
 				end
 			else
 				TriggerClientEvent("esx:showNotification", src, "This time is invalid!")
 			end
 		else
-			TriggerClientEvent("esx:showNotification", src, "This ID is not online!")
+			print(
+				string.format(
+					"^2%s^7 -> [^1%s^7] ^1%s^7 has attempted to place [^5%s^7] ^5%s^7 into jail via the ^2jail^7 command. The legitimacy check returned ^1false^7 with the reason of ^2%s^7.",
+					GetCurrentResourceName(), src, GetPlayerName(src), tgt, GetPlayerName(tgt), legit["reason"]
+				)
+			)
 		end
 	else
-		TriggerClientEvent("esx:showNotification", src, "You are not an officer!")
+		TriggerClientEvent("esx:showNotification", src, "This ID is not online!")
 	end
+
 end)
 
-RegisterCommand("unjail", function(src, args)
-
-	local xPlayer = ESX.GetPlayerFromId(src)
-
-	if xPlayer["job"]["name"] == "police" then
-
-		local jailPlayer = args[1]
-
-		if GetPlayerName(jailPlayer) ~= nil then
-			UnJail(jailPlayer)
+RegisterCommand("unjail", function(source, args)
+	local src, tgt = source, tonumber(args[1]);
+	if GetPlayerName(tgt) ~= nil then
+		local legit = checkIfLegit(src, tgt);
+		if legit["legit"] == true then
+			UnJail(tgt)
 		else
-			TriggerClientEvent("esx:showNotification", src, "This ID is not online!")
+			print(
+				string.format(
+					"^2%s^7 -> [^1%s^7] ^1%s^7 has attempted to remove [^5%s^7] ^5%s^7 from jail via the ^2unjail^7 command. The legitimacy check returned ^1false^7 with the reason of ^2%s^7.",
+					GetCurrentResourceName(), src, GetPlayerName(src), tgt, GetPlayerName(tgt), legit["reason"]
+				)
+			)
 		end
 	else
-		TriggerClientEvent("esx:showNotification", src, "You are not an officer!")
+		TriggerClientEvent("esx:showNotification", src, "This ID is not online!")
 	end
 end)
 
 RegisterServerEvent("esx-qalle-jail:jailPlayer")
-AddEventHandler("esx-qalle-jail:jailPlayer", function(targetSrc, jailTime, jailReason)
-	local src = source
-	local targetSrc = tonumber(targetSrc)
+AddEventHandler("esx-qalle-jail:jailPlayer", function(t, jT, jR)
+	local src, tgt = source, t;
+	local jailTime, jailReason = jT, jR;
 
-	JailPlayer(targetSrc, jailTime)
+	local legit = checkIfLegit(src, tgt);
+	if legit["legit"] == true then
+		JailPlayer(tgt, jailTime)
 
-	GetRPName(targetSrc, function(Firstname, Lastname)
-		TriggerClientEvent('chat:addMessage', -1, { args = { "JUDGE",  Firstname .. " " .. Lastname .. " Is now in jail for the reason: " .. jailReason }, color = { 249, 166, 0 } })
-	end)
-
-	TriggerClientEvent("esx:showNotification", src, GetPlayerName(targetSrc) .. " Jailed for " .. jailTime .. " minutes!")
-end)
-
-RegisterServerEvent("esx-qalle-jail:unJailPlayer")
-AddEventHandler("esx-qalle-jail:unJailPlayer", function(targetIdentifier)
-	local src = source
-	local xPlayer = ESX.GetPlayerFromIdentifier(targetIdentifier)
-
-	if xPlayer ~= nil then
-		UnJail(xPlayer.source)
+		GetRPName(tgt, function(Firstname, Lastname)
+			TriggerClientEvent("esx:showNotification", src, Firstname .. " " .. Lastname .. " has been sentenced to " .. jailTime .. " minutes in jail.")
+			TriggerClientEvent('chat:addMessage', -1, { args = { "JUDGE",  Firstname .. " " .. Lastname .. " has been sentenced to " .. jailTime .. " minutes in jail for the charges of " .. jailReason .. "." }, color = { 249, 166, 0 } })
+		end)
 	else
-		MySQL.Async.execute(
-			"UPDATE users SET jail = @newJailTime WHERE identifier = @identifier",
-			{
-				['@identifier'] = targetIdentifier,
-				['@newJailTime'] = 0
-			}
+		print(
+			string.format(
+				"^2%s^7 -> [^1%s^7] ^1%s^7 has attempted to remove [^5%s^7] ^5%s^7 from jail via the ^2jailPlayer^7 event. The legitimacy check returned ^1false^7 with the reason of ^2%s^7.",
+				GetCurrentResourceName(), src, GetPlayerName(src), tgt, GetPlayerName(tgt), legit["reason"]
+			)
 		)
 	end
 
-	TriggerClientEvent("esx:showNotification", src, xPlayer.name .. " Unjailed!")
+end)
+
+RegisterServerEvent("esx-qalle-jail:unJailPlayer")
+AddEventHandler("esx-qalle-jail:unJailPlayer", function(tI)
+	local src = source;
+	local targetIdentifier = tI;
+	local xTgt = ESX.GetPlayerFromIdentifier(targetIdentifier);
+	local tgt = xTgt.source;
+	if xTgt ~= nil then
+		local legit = checkIfLegit(src, tgt);
+		if legit["legit"] == true then
+			UnJail(tgt)
+			GetRPName(tgt, function(Firstname, Lastname)
+				TriggerClientEvent("esx:showNotification", src, Firstname .. " " .. Lastname .. " has been released from jail.")
+				TriggerClientEvent('chat:addMessage', -1, { args = { "JUDGE",  Firstname .. " " .. Lastname .. " has been released from jail." }, color = { 249, 166, 0 } })
+			end)
+		else
+			print(
+				string.format(
+					"^2%s^7 -> [^1%s^7] ^1%s^7 has attempted to remove [^5%s^7] ^5%s^7 from jail via the ^2unJailPlayer^7 event. The legitimacy check returned ^1false^7 with the reason of ^2%s^7.",
+					GetCurrentResourceName(), src, GetPlayerName(src), tgt, GetPlayerName(tgt), legit["reason"]
+				)
+			)
+		end
+	end
 end)
 
 RegisterServerEvent("esx-qalle-jail:updateJailTime")
@@ -97,12 +152,23 @@ end)
 RegisterServerEvent("esx-qalle-jail:prisonWorkReward")
 AddEventHandler("esx-qalle-jail:prisonWorkReward", function()
 	local src = source
-
 	local xPlayer = ESX.GetPlayerFromId(src)
-
-	xPlayer.addMoney(math.random(13, 21))
-
-	TriggerClientEvent("esx:showNotification", src, "Thanks, here you have som cash for food!")
+	
+	local sql = MySQL.Sync.fetchScalar("SELECT jail FROM users WHERE identifier = @identifier", { ["@identifier"] = xPlayer.identifier })
+	if sql ~= nil then
+		sql = tonumber(sql);
+		if sql > 0 then
+			xPlayer.addMoney(math.random(13, 21))
+			TriggerClientEvent("esx:showNotification", src, "Thanks, here you have some cash for food!")
+		else
+			print(
+				string.format(
+					"^2%s^7 -> [^1%s^7] ^1%s^7 has attempted to exploit money through the ^2prisonWorkReward^7 event.",
+					GetCurrentResourceName(), src, GetPlayerName(src)
+				)
+			)
+		end
+	end
 end)
 
 function JailPlayer(jailPlayer, jailTime)
